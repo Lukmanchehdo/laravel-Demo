a324f6837f499364ab6dd0abb2ec8b64c0761aa2
Lukmanchehdo <31103102+Lukmanchehdo@users.noreply.github.com>
2021-10-08 22:01:18 +0700

    store to table

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -35,7 +35,36 @@ class UrlController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request->all());
+        //TODO::1
+        /* Url::create([
+        'url' => $request->url,
+        'code' => 'XYZ'
+        ]);*/
+
+        //TODO::2
+        /*Url::insert([
+        'url' => $request->url,
+        'code' => 'XYZ'
+        ]);*/
+
+        //TODO::3
+        /*Url::forceCreate([
+        'url'  => $request->url,
+        'code' => 'XYZ'
+        ]);*/
+
+        //TODO::4
+        $model_url = new Url();
+        $model_url->url = $request->url;
+        $model_url->code = 'XYZ';
+        $model_url->save();
+
+        //update
+        // $model_url = Url::query()->find(4);
+        // $model_url->code = 'ABC';
+        // $model_url->save();
+
     }
 
     /**
--- resources/views/urls/add.blade.php
+++ resources/views/urls/add.blade.php
@@ -1,18 +1,21 @@
 @extends('app')
 
 @section('content')
-    <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-24 mx-auto">
-            <div class="text-center text-2xl font-bold text-green-600">
-                THE LINK KNOWS ALL. SO CAN YOU.
-            </div>
-            <div class="w-1/2 mx-auto mt-10">
-                <input type="text"
-                       id="url"
-                       placeholder="กรุณาใส่ URL นาจา"
-                       name="url"
-                       class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-            </div>
+<section class="text-gray-600 body-font relative">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="text-center text-2xl font-bold text-green-600">
+            THE LINK KNOWS ALL. SO CAN YOU.
         </div>
-    </section>
+        <form action="{{ route("url.store") }}" method="POST">
+            @csrf
+            <div class="w-1/2 mx-auto mt-10">
+                <input type="text" id="url" placeholder="กรุณาใส่ URL นาจา" name="url"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <button type="submit"
+                    class="inline-flex items-center bg-green-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-green-700 rounded text-base mt-4">สร้าง
+                    URL</button>
+            </div>
+        </form>
+    </div>
+</section>
 @endsection
